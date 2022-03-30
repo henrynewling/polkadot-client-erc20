10a5c32152cb5a46e93b04f7856037ebaab6b6e5
henry newling <henrynewling@gmail.com>
2022-03-30 14:26:42 +0545

    added metadata and some contract methods

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { ApiPromise, WsProvider } from '@polkadot/api';
 import './App.css';
-import { web3Accounts, web3Enable } from '@polkadot/extension-dapp';
+import { web3Accounts, web3Enable, web3FromSource } from '@polkadot/extension-dapp';
+import abi from './metadata.json';
+import { ContractPromise } from '@polkadot/api-contract';
 
+console.log(abi);
 
 const PROVIDER_URL = 'wss://frost-rpc.icenetwork.io:9944';
 
 function App() {
   const [api, setApi] = useState(null);
   const [accounts, setAccounts] = useState([]);
+  const [token, setToken] = useState({});
+  const [userLogin, setUserLogin] = useState(false);
+  const [contract, setContract] = useState(null);
+  const [methods, setMethods] = useState([]);
+  const [hash, setHash] = useState({});
+  const [loading, setLoading] = useState('');
+  const addressRef = useRef(null);
+  const targetAddressRef = useRef(null);
+  const transferAmountRef = useRef(null);
+
+  const CONTRACT_ADDRESS = '5FZ4bns2YnroQYjGXPCTG2CxitiZeYK9LeU5KnkLEeSC49jz';
 
   const extensionSetup = async () => {
     const extension = await web3Enable('polkadot-client-app');
@@ -17,7 +31,14 @@ function App() {
       return;
     }
     let acc = await web3Accounts();
+    // initialize contract
+    const contractAddress = CONTRACT_ADDRESS;
+    const contract = new ContractPromise(api, abi, contractAddress );
+    console.log(contract)
+    setMethods(abi.messages);
+    setContract(contract);
     setAccounts(acc);
+    setUserLogin(true);
 
   }
 
@@ -29,10 +50,94 @@ function App() {
       if(_api) {
         console.log('Connection Success');
       }
-      await extensionSetup();
     } catch (error) {
       console.log(error);
     }
+  }
+
+  const gasLimit = 20000n * 1000000n;
+  const value = 0;
+
+  const getTokenName = async () => {
+    setLoading('Reading...');
+    const {result,output} = await contract.query.name('',{ value, gasLimit });
+    if(result.isOk){
+      setToken({
+        ...token,
+        name: output.toHuman()
+      });
+      setLoading('');
+    } else {
+      setLoading('');
+      console.log('Error:', result.asErr);
+    }
+  }
+  const getTokenSymbol = async () => {
+    setLoading('Reading...');
+    const {result,output} = await contract.query.symbol('',{ value, gasLimit });
+    if(result.isOk){
+      setToken({
+        ...token,
+        symbol: output.toHuman()
+      })
+      setLoading('');
+    } else {
+      setLoading('');
+      console.log('Error:', result.asErr);
+    }
+  }
+
+  const getTotalSupply = async () => {
+    setLoading('Reading...');
+    const {result,output} = await contract.query.totalSupply('',{ value, gasLimit });
+    if(result.isOk){
+      setToken({
+        ...token,
+        totalSupply: output.toHuman()
+      })
+      setLoading('');
+    } else {
+      setLoading('');
+      console.log('Error:', result.asErr);
+    }
+  }
+  const getBalance = async () => {
+    try {
+      setLoading('Reading...');
+      const {result,output} = await contract.query.balanceOf(accounts[0].address,{value, gasLimit: -1}, addressRef.current.value);
+      if(result.isOk){
+        setToken({
+          ...token,
+          balance: output.toHuman()
+        })
+        setLoading('');
+      } else {
+        setLoading('');
+        console.log('Error:', result.asErr);
+      }
+    } catch (error) {
+      setLoading('');
+      console.log(error)
+    }
+    
+  }
+  const transfer = async () => {
+    try {
+      setLoading('Executing...');
+      const injector = await web3FromSource(accounts[0].meta.source);
+      await contract.tx.transfer({value, gasLimit}, targetAddressRef.current.value, transferAmountRef.current.value).signAndSend(accounts[0].address, {signer: injector.signer}, ({status}) => {
+        if(status.isFinalized) {
+          console.log(`Finalized. Block hash: ${status.asFinalized.toString()}`);
+          setLoading('');
+        } else {
+          console.log(`Current transaction status: ${status.type}`);
+        }
+      })
+    } catch (error) {
+      setLoading('');
+      console.log(error)
+    }
+    
   }
 
   useEffect(() => {
@@ -40,8 +145,41 @@ function App() {
   },[])
   return (
     <div className="App">
-      Polkadot app
-      <button onClick={() => console.log(api)}>Click</button>
+      <h3>ERC20 Token</h3>
+      {accounts.length !== 0 ? (
+        <p>Polkadot Wallet Address: <strong>{accounts[0].address}</strong></p>
+      ): (
+        <button disabled={!api ? true : false} onClick={extensionSetup}>Login</button>
+      )}
+      {loading.length !== 0 && (
+          <span>{loading}</span>
+      )}
+      {userLogin && (
+        <div className="methods">
+        <div className="method name">
+          <button disabled={!api ? true : false} onClick={getTokenName}>Name</button>
+          <p>{token?.name}</p>
+        </div>
+        <div className="method symbol">
+          <button disabled={!api ? true : false} onClick={getTokenSymbol}>Symbol</button>
+          <p>{token?.symbol}</p>
+        </div>
+        <div className="method supply">
+          <button disabled={!api ? true : false} onClick={getTotalSupply}>Total Supply</button>
+          <p>{token?.totalSupply}</p>
+        </div>
+        <div className="method balaceOf">
+          <input type="text" placeholder='Enter wallet address' ref={addressRef} />
+          <button disabled={!api ? true : false} onClick={getBalance}>get Balance</button>
+          <p>{token?.balance}</p>
+        </div>
+        <div className="method transfer">
+          <input type="text" placeholder='Enter target address' ref={targetAddressRef} />
+          <input type="number" placeholder='Enter amount' ref={transferAmountRef} />
+          <button disabled={!api ? true : false} onClick={transfer}>transfer</button>
+        </div>
+      </div>
+      )}
     </div>
   );
 }
