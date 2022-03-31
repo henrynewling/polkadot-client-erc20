7d4280796ff3438fe19ef0a43566a024a151911c
henry newling <henrynewling@gmail.com>
2022-03-31 13:00:46 +0545

    added and tested all remaining methods

--- src/App.js
+++ src/App.js
@@ -5,24 +5,42 @@ import { web3Accounts, web3Enable, web3FromSource } from '@polkadot/extension-da
 import abi from './metadata.json';
 import { ContractPromise } from '@polkadot/api-contract';
 
-console.log(abi);
 
 const PROVIDER_URL = 'wss://frost-rpc.icenetwork.io:9944';
+const CONTRACT_ADDRESS = '5FZ4bns2YnroQYjGXPCTG2CxitiZeYK9LeU5KnkLEeSC49jz';
 
 function App() {
   const [api, setApi] = useState(null);
   const [accounts, setAccounts] = useState([]);
   const [token, setToken] = useState({});
   const [userLogin, setUserLogin] = useState(false);
+  const [userWallet, setUserWallet] = useState(null);
   const [contract, setContract] = useState(null);
-  const [methods, setMethods] = useState([]);
-  const [hash, setHash] = useState({});
   const [loading, setLoading] = useState('');
-  const addressRef = useRef(null);
-  const targetAddressRef = useRef(null);
-  const transferAmountRef = useRef(null);
+  const [inputParam, setInputParam] = useState({
+    ownerAddress: '',
+    spenderAddress: '',
+    checkBalanceAddress: '',
+    targetAddress: '',
+    approveAddress: '',
+    transferFromAddress: '',
+    transferToAddress: '',
+    transferAmount: 0,
+    approveAmount: 0,
+    transferFromAmount: 0
+  })
 
-  const CONTRACT_ADDRESS = '5FZ4bns2YnroQYjGXPCTG2CxitiZeYK9LeU5KnkLEeSC49jz';
+  const { ownerAddress,spenderAddress, checkBalanceAddress, targetAddress, approveAddress, transferFromAmount, transferFromAddress, transferToAddress, approveAmount, transferAmount } = inputParam;
+
+
+ 
+
+  const handleChange = (e) => {
+    setInputParam({
+      ...inputParam,
+      [e.target.name]: e.target.value
+    })
+  }
 
   const extensionSetup = async () => {
     const extension = await web3Enable('polkadot-client-app');
@@ -34,21 +52,25 @@ function App() {
     // initialize contract
     const contractAddress = CONTRACT_ADDRESS;
     const contract = new ContractPromise(api, abi, contractAddress );
-    console.log(contract)
-    setMethods(abi.messages);
+    console.log(contract);
     setContract(contract);
     setAccounts(acc);
-    setUserLogin(true);
 
+  }
+
+  const login = (e) => {
+    console.log(e.target.value)
+    setUserWallet(e.target.value);
+    setUserLogin(true);
   }
 
   const setup = async () => {
     try {
       const wsProvider = new WsProvider(PROVIDER_URL);
       const _api = await ApiPromise.create({ provider: wsProvider });
-      setApi(_api);
       if(_api) {
         console.log('Connection Success');
+        setApi(_api);
       }
     } catch (error) {
       console.log(error);
@@ -60,7 +82,7 @@ function App() {
 
   const getTokenName = async () => {
     setLoading('Reading...');
-    const {result,output} = await contract.query.name('',{ value, gasLimit });
+    const {result,output} = await contract.query.name(userWallet,{ value, gasLimit });
     if(result.isOk){
       setToken({
         ...token,
@@ -74,7 +96,7 @@ function App() {
   }
   const getTokenSymbol = async () => {
     setLoading('Reading...');
-    const {result,output} = await contract.query.symbol('',{ value, gasLimit });
+    const {result,output} = await contract.query.symbol(userWallet,{ value, gasLimit });
     if(result.isOk){
       setToken({
         ...token,
@@ -89,7 +111,7 @@ function App() {
 
   const getTotalSupply = async () => {
     setLoading('Reading...');
-    const {result,output} = await contract.query.totalSupply('',{ value, gasLimit });
+    const {result,output} = await contract.query.totalSupply(userWallet,{ value, gasLimit });
     if(result.isOk){
       setToken({
         ...token,
@@ -101,10 +123,30 @@ function App() {
       console.log('Error:', result.asErr);
     }
   }
-  const getBalance = async () => {
+  const getAllowance = async () => {
     try {
       setLoading('Reading...');
-      const {result,output} = await contract.query.balanceOf(accounts[0].address,{value, gasLimit: -1}, addressRef.current.value);
+      const {result,output} = await contract.query.allowance(userWallet,{ value, gasLimit },ownerAddress, spenderAddress);
+      if(result.isOk){
+        setToken({
+          ...token,
+          allowance: output.toHuman()
+        })
+        setLoading('');
+      } else {
+        setLoading('');
+        console.log('Error:', result.asErr);
+      }
+    } catch (error) {
+      setLoading('');
+      console.log(error)
+    }
+  }
+  const getBalance = async () => {
+    try {
+      console.log(checkBalanceAddress, 'Bal')
+      setLoading('Reading...');
+      const {result,output} = await contract.query.balanceOf(userWallet,{value, gasLimit: -1}, checkBalanceAddress);
       if(result.isOk){
         setToken({
           ...token,
@@ -123,9 +165,53 @@ function App() {
   }
   const transfer = async () => {
     try {
+      console.log(transferAmount,":amt");
+      console.log("add:",targetAddress)
       setLoading('Executing...');
-      const injector = await web3FromSource(accounts[0].meta.source);
-      await contract.tx.transfer({value, gasLimit}, targetAddressRef.current.value, transferAmountRef.current.value).signAndSend(accounts[0].address, {signer: injector.signer}, ({status}) => {
+      const fromAcc = accounts.find(a => a.address === userWallet);
+      console.log(fromAcc)
+      const injector = await web3FromSource(fromAcc.meta.source);
+      await contract.tx.transfer({value, gasLimit}, targetAddress, transferAmount).signAndSend(fromAcc.address, {signer: injector.signer}, ({status}) => {
+        if(status.isFinalized) {
+          console.log(`Finalized. Block hash: ${status.asFinalized.toString()}`);
+          setLoading('');
+        } else {
+          console.log(`Current transaction status: ${status.type}`);
+        }
+      })
+    } catch (error) {
+      setLoading('');
+      console.log(error)
+    }
+    
+  }
+
+  const approve = async () => {
+    try {
+      setLoading('Executing...');
+      const fromAcc = accounts.find(a => a.address === userWallet);
+      const injector = await web3FromSource(fromAcc.meta.source);
+      await contract.tx.approve({value, gasLimit},approveAddress, approveAmount).signAndSend(fromAcc.address, {signer: injector.signer}, ({status}) => {
+        if(status.isFinalized) {
+          console.log(`Finalized. Block hash: ${status.asFinalized.toString()}`);
+          setLoading('');
+        } else {
+          console.log(`Current transaction status: ${status.type}`);
+        }
+      })
+    } catch (error) {
+      setLoading('');
+      console.log(error)
+    }
+    
+  }
+
+  const transferFrom = async () => {
+    try {
+      setLoading('Executing...');
+      const fromAcc = accounts.find(a => a.address === userWallet);
+      const injector = await web3FromSource(fromAcc.meta.source);
+      await contract.tx.transferFrom({value, gasLimit}, transferFromAddress, transferToAddress, transferFromAmount).signAndSend(fromAcc.address, {signer: injector.signer}, ({status}) => {
         if(status.isFinalized) {
           console.log(`Finalized. Block hash: ${status.asFinalized.toString()}`);
           setLoading('');
@@ -147,37 +233,73 @@ function App() {
     <div className="App">
       <h3>ERC20 Token</h3>
       {accounts.length !== 0 ? (
-        <p>Polkadot Wallet Address: <strong>{accounts[0].address}</strong></p>
+        <>
+          <p>Select the account to login</p>
+          <select onChange={login}>
+            <option value="">Select Account</option>
+            {accounts.map((account,idx) => (
+              <option key={idx} value={account.address}>{account.address}</option>
+            ))}
+          </select>
+        </>
       ): (
         <button disabled={!api ? true : false} onClick={extensionSetup}>Login</button>
+      )}
+      {userLogin && (
+        <p>Polkadot Wallet Address: <strong>{userWallet}</strong></p>
       )}
       {loading.length !== 0 && (
           <span>{loading}</span>
       )}
       {userLogin && (
         <div className="methods">
-        <div className="method name">
-          <button disabled={!api ? true : false} onClick={getTokenName}>Name</button>
-          <p>{token?.name}</p>
-        </div>
-        <div className="method symbol">
-          <button disabled={!api ? true : false} onClick={getTokenSymbol}>Symbol</button>
-          <p>{token?.symbol}</p>
-        </div>
-        <div className="method supply">
-          <button disabled={!api ? true : false} onClick={getTotalSupply}>Total Supply</button>
-          <p>{token?.totalSupply}</p>
-        </div>
-        <div className="method balaceOf">
-          <input type="text" placeholder='Enter wallet address' ref={addressRef} />
-          <button disabled={!api ? true : false} onClick={getBalance}>get Balance</button>
-          <p>{token?.balance}</p>
-        </div>
-        <div className="method transfer">
-          <input type="text" placeholder='Enter target address' ref={targetAddressRef} />
-          <input type="number" placeholder='Enter amount' ref={transferAmountRef} />
-          <button disabled={!api ? true : false} onClick={transfer}>transfer</button>
-        </div>
+          <div className="method name">
+            <h3>Name</h3>
+            <button disabled={!api ? true : false} onClick={getTokenName}>Read</button>
+            <p>{token?.name}</p>
+          </div>
+          <div className="method symbol">
+            <h3>Symbol</h3>
+            <button disabled={!api ? true : false} onClick={getTokenSymbol}>Read</button>
+            <p>{token?.symbol}</p>
+          </div>
+          <div className="method supply">
+            <h3>Total Supply</h3>
+            <button disabled={!api ? true : false} onClick={getTotalSupply}>Read</button>
+            <p>{token?.totalSupply}</p>
+          </div>
+          <div className="method allowance">
+            <h3>Allowance</h3>
+            <input type="text" placeholder='Enter owner address' name='ownerAddress' value={ownerAddress} onChange={handleChange}  />
+            <input type="text" placeholder='Enter spender address' name='spenderAddress' value={spenderAddress} onChange={handleChange}  />
+            <button disabled={!api ? true : false} onClick={getAllowance}>Read</button>
+            <p>{token?.allowance}</p>
+          </div>
+          <div className="method balaceOf">
+            <h3>Check Balance</h3>
+            <input type="text" placeholder='Enter wallet address' name='checkBalanceAddress' value={checkBalanceAddress} onChange={handleChange}  />
+            <button disabled={!api ? true : false} onClick={getBalance}>Read</button>
+            <p>{token?.balance}</p>
+          </div>
+          <div className="method transfer">
+            <h3>Transfer</h3>
+            <input type="text" placeholder='Enter target address' name='targetAddress'  value={targetAddress} onChange={handleChange} />
+            <input type="number" placeholder='Enter amount' name='transferAmount'  value={transferAmount} onChange={handleChange}  />
+            <button disabled={!api ? true : false} onClick={transfer}>Execute</button>
+          </div>
+          <div className="method approve">
+            <h3>Approve</h3>
+            <input type="text" placeholder='Enter spender address' name='approveAddress'  value={approveAddress} onChange={handleChange}  />
+            <input type="number" placeholder='Enter amount' name='approveAmount'  value={approveAmount} onChange={handleChange}  />
+            <button disabled={!api ? true : false} onClick={approve}>Execute</button>
+          </div>
+          <div className="method transferFrom">
+            <h3>Transfer from</h3>
+            <input type="text" placeholder='From address' name='transferFromAddress'  value={transferFromAddress} onChange={handleChange}  />
+            <input type="text" placeholder='to address' name='transferToAddress'  value={transferToAddress} onChange={handleChange}  />
+            <input type="number" placeholder='Enter amount' name='transferFromAmount'  value={transferFromAmount} onChange={handleChange}  />
+            <button disabled={!api ? true : false} onClick={transferFrom}>Execute</button>
+          </div>
       </div>
       )}
     </div>
